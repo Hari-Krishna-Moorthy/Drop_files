e333225ad641eba2768d7a56213b0c8a4bd74711
Hari <harikrishnamoorthy@yahoo.com>
2021-07-20 22:52:17 +0530

    adding urls.py for hosting

--- drop_files/urls.py
+++ drop_files/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
     path('', include('accounts.urls')),
     path('', include('upload.urls')),
     path('', home_view ),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
